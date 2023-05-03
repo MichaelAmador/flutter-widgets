30b38468c933816e809ca2f81a02ce86176a411f
Michael Amador Centeno <57235798+MichaelAmador@users.noreply.github.com>
2023-05-03 15:41:56 -0600

    Card finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'config/router/app_router.dart';
 import 'config/themes/app_theme.dart';
 
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
   runApp(const MainApp());
 }
 
@@ -16,5 +19,14 @@ class MainApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColor: 2).getTheme(),
     );
+  }
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
   }
 }
