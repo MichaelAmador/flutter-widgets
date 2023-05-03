ee11f9bdb15b7641b4c271000069bf7b41afa840
Michael Amador Centeno <57235798+MichaelAmador@users.noreply.github.com>
2023-05-03 15:52:03 -0600

    Add screens

--- lib/presentations/screens/screens.dart
+++ lib/presentations/screens/screens.dart
@@ -1,3 +1,9 @@
+export 'package:widgets/presentations/screens/animated/animated_screen.dart';
+export 'package:widgets/presentations/screens/app_tutorial/app_tutorial.dart';
 export 'package:widgets/presentations/screens/buttons/buttons_screen.dart';
 export 'package:widgets/presentations/screens/cards/cards_screen.dart';
 export 'package:widgets/presentations/screens/home/home_screen.dart';
+export 'package:widgets/presentations/screens/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:widgets/presentations/screens/progress/progress_screen.dart';
+export 'package:widgets/presentations/screens/snackbar/snackbar_screen.dart';
+export 'package:widgets/presentations/screens/ui_controls/ui_controls.dart';
---
+++ lib/presentations/screens/snackbar/snackbar_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SnackbackScreen extends StatelessWidget {
+  static const name = 'snackbar_screen';
+  const SnackbackScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
